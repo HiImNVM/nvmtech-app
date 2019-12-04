33d1c8405d63e35469a1ffe4c80645ff898f088f
crystalng285 <tramng123>
2019-12-04 21:58:02 +0700

    add forgot password text and change some function

--- lib/src/constants/login_constant.dart
+++ lib/src/constants/login_constant.dart
@@ -1,3 +1,5 @@
+const String CONST_FORGOTPASSTITLE = 'Forgot Password';
+
 const String CONST_LOGINTEXT_OR = 'Or';
 const String CONST_LOGINTEXT_SIGNIN = " have an account? ";
 const String CONST_LOGINTEXT_SIGNINTITLE = "Sign In";
